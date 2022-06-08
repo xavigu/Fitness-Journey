beb4ceb0dbc635d474b44d6962d5920e459886a0
xavigu <xavigu91@gmail.com>
2022-06-08 16:53:42 +0200

    change logo

--- src/app/header/app-header.component.ts
+++ src/app/header/app-header.component.ts
@@ -9,7 +9,7 @@ import { User } from '../auth/shared/services/auth/auth.service';
   template: `
     <div class="app-header">
       <div class="wrapper">
-        <img src="/assets/logo.svg">
+        <img src="/assets/logo.png">
         <div 
           class="app-header__user-info"
           *ngIf="user?.authenticated">
